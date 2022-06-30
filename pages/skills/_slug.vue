653569fe418464a23c1cca5dc10c12740894b350
aleafwolf <aleafwolf@outlook.com>
2022-06-29 23:51:33 -0400

    test layout for /skills

--- pages/skills/_slug.vue
+++ pages/skills/_slug.vue
@@ -1,31 +1,65 @@
 <template>
-  <div>
-    <!-- <h2>{{skill.data.attributes.name}}</h2> -->
+  <div class="wrapper">
+    <h2 class="text-center text-xl">{{ skill.name }}</h2>
+    <div class="flex w-full">
+      <NuxtLink
+        v-for="(s, i) in skill.skill_group.skills"
+        :key="i"
+        :to="`/skills/${s.slug}`"
+        class="bg-gray-300 text-center flex-1 mx-3"
+        :class="{skillSelected: skill.slug === s.slug}"
+      >
+        {{ s.rank }} {{ s.variant }}
+      </NuxtLink>
+    </div>
+    <div class="flex justify-center items-center flex-col text-center">
+      <img
+        style="width: 80px; height: 80px"
+        :src="`${imgUrl}/技能/${skill.img_ref}.webp`"
+        :alt="skill.name"
+      />
+      <div>{{ skill.description }}</div>
+      <div>{{ skill.number.lv1 }} / {{ skill.number.lv10 }}</div>
+      <div class="flex">
+        <NuxtLink v-for="(card, j) in skill.relate_cards" :key="j" :to="`/cards/${card.slug}`">
+          <img
+            style="width: 80px; height: 80px"
+            :src="`${imgUrl}/${card.character.name}/${card.img_ref}.webp`"
+            :alt="skill.name"
+          />
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      imgUrl: this.$globalV.ihs,
+    };
+  },
   async asyncData({ $axios, route, app }) {
     const slug = route.params.slug;
     const skill = await $axios
-      .$get(`/api/skills?filters[slug][$eq]=${slug}`, {
+      .$get(`/api/skill/detail`, {
         params: {
-        //   filters: {
-        //     slug: {
-        //       $eq: "sheng-xian-duo-shi-2",
-        //     },
-        //   },
-        //   populate: "*",
-        locale: app.i18n.locale
+          slug: slug,
+          locale: app.i18n.locale,
         },
       })
       .catch((err) => {
         console.log(err);
       });
     return {
-      skill: skill.data,
+      skill: skill[0],
     };
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .skillSelected{
+    background-color: blue;
+    color: white;
+  }
+</style>
