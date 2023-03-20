0e4a4867563c10a6eed8ff9ff23cd843aef85432
aleafwolf <aleafwolf@outlook.com>
2023-03-20 10:19:56 -0400

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nxxtoolbox-nuxt",
+    title: "NXXToolbox",
     htmlAttrs: {
       lang: "zh",
     },
