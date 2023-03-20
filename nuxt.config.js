1a4e9ffabee6af0d4dcc6dee913fb90339cb3ce5
aleafwolf <aleafwolf@outlook.com>
2023-03-20 10:15:16 -0400

    add config for pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     "@nuxtjs/tailwindcss",
+    '@nuxtjs/pwa',
   ],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
